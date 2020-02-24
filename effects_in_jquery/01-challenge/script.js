c56dfc4bde8503b06dce802aa9347e36629166d3
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-02-24 22:45:18 +0000

    jquery challenges

--- effects_in_jquery/01-challenge/script.js
+++ effects_in_jquery/01-challenge/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
     $("#button_effects1").click(function(){
             $('#button_effects1').hide('slow');
+        });
+});
 
+$(document).ready(function() {
+    $("#button_effects1").click(function(){
+            $('#button_effects1').hide('medium');
+        });
+});
+
+$(document).ready(function() {
+    $("#button_effects1").click(function(){
+            $('#button_effects1').hide('fast');
+        });
+});
+
+$(document).ready(function() {
+    $("#button_effects1").click(function(){
+            $('#button_effects1').hide(3000);
         });
 });
